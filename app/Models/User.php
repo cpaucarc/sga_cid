92233164c384503bb56f13c77e372a2cda58732a
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-08-01 18:51:28 -0500

    :seedling: Agrega seeder de usuario

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
      *
      * @var string[]
      */
-    protected $fillable = ['name', 'email', 'password',];
+    protected $fillable = ['name', 'username', 'email', 'password', 'esta_activo', 'persona_id'];
 
     /**
      * The attributes that should be hidden for serialization.
