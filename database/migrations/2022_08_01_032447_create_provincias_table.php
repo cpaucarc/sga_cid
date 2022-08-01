ddbc564d5c260e05ea32f4c962e94c2a932771e3
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-07-31 22:35:06 -0500

    :card_file_box: Agrega migraciones de nivel 2

---
+++ database/migrations/2022_08_01_032447_create_provincias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provincias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->foreignId('departamento_id')->constrained('departamentos')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provincias');
+    }
+};
