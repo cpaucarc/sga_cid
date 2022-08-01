b5e41511930a23659112832a31993908faa859a7
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-08-01 00:25:41 -0500

    :card_file_box: Agrega las migraciones del nivel 8

---
+++ database/migrations/2022_08_01_052042_create_pagos_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pagos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nro_comprobante', 20);
+            $table->date('fecha_pago');
+            $table->decimal('monto_pagado');
+            $table->decimal('monto_establecido');
+            $table->smallInteger('tipo_estudiante_id');
+            $table->boolean('esta_revisado')->default(false);
+            $table->boolean('esta_validado')->default(false);
+            $table->smallInteger('pago_lugar_id');
+            $table->foreignId('matriculado_id')->constrained('matriculados')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignId('documento_id')->constrained('documentos')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->text('observacion');
+            $table->date('fecha_revision');
+            $table->date('fecha_validacion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pagos');
+    }
+};
