975e6f8d4bce3dba3c8515d15d022d07132c8b33
Frank Paucar <fcpaucar@gmail.com>
2022-08-01 09:00:37 -0500

    :seedling: Agrega los seeders al archivo de ejecución

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        /* TODO: Colocar en su respectivo nivel, y por orden alfabetico  */
+        // Nivel 0
+
+        $this->call(PaisSeeder::class);
+
+        // Nivel 1
+
+        $this->call(DepartamentoSeeder::class);
+
+        // Nivel 2
+
+        $this->call(ProvinciaSeeder::class);
+
+        // Nivel 3
+
+        $this->call(DistritoSeeder::class);
+
+        // Nivel 4
+        // Nivel 5
+        // Nivel 6
+        // Nivel 7
+        // Nivel 8
+        // Nivel 9
+
+
     }
 }
