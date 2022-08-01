961b47c7b99eb1eecf8ada0b303f2525a09732ff
Frank Paucar <fcpaucar@gmail.com>
2022-08-01 18:17:21 -0500

    :card_file_box: Elimina la columna 'username'

--- database/migrations/2022_08_01_040000_create_users_table.php
+++ database/migrations/2022_08_01_040000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -23,7 +22,7 @@ return new class extends Migration {
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->boolean('esta_activo')->default(true);
-            $table->foreignId('persona_id')->constrained('personas')
+            $table->foreignId('persona_id')->nullable()->constrained('personas')
                 ->cascadeOnUpdate()
                 ->cascadeOnDelete();
             $table->timestamps();
