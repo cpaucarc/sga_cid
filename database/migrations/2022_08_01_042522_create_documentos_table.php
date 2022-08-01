0928793983d617ed3f385c310b188baf6ede0c11
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-07-31 23:37:26 -0500

    :card_file_box: Agrega las migraciones del nivel 6

---
+++ database/migrations/2022_08_01_042522_create_documentos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('ruta');
+            $table->string('peso');
+            $table->foreignId('user_id')->nullable()->constrained('users')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documentos');
+    }
+};
