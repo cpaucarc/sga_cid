4fbc790cc52c7b78f9be3b7ba0d51415d718583f
Frank Paucar <fcpaucar@gmail.com>
2022-08-02 11:51:40 -0500

    :lipstick: Reemplaza el color de fondo de gray-100  a white

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <body class="font-sans antialiased">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-white">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
