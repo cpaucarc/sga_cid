bd6dde4974469cd787edf491d4af617150a56906
Frank Paucar <fcpaucar@gmail.com>
2022-08-02 19:13:23 -0500

    :sparkles: Agrega los componentes de Alerta y Titulo

---
+++ resources/views/components/alerta.blade.php
@@ -0,0 +1,6 @@
+<div class="bg-amber-300 rounded-md px-4 py-3 flex items-center gap-x-4 text-sm text-slate-900">
+    <x-icons.warning class="icon-6"/>
+    <div {{ $attributes }}>
+        {{ $slot }}
+    </div>
+</div>
---
+++ resources/views/components/titulo.blade.php
@@ -0,0 +1,19 @@
+<div class="mb-8 -mt-2 border-b border-dashed border-slate-300 pb-4">
+    <div class="flex items-center justify-between gap-x-6">
+        <div>
+            <h1 class="font-bold text-slate-900 text-xl">
+                {{ $titulo }}
+            </h1>
+            @if(isset($subtitulo))
+                <p class="text-slate-500 text-sm">
+                    {{ $subtitulo }}
+                </p>
+            @endif
+        </div>
+        @if(isset($items))
+            <div class="flex items-center gap-x-2 justify-end">
+                {{ $items }}
+            </div>
+        @endif
+    </div>
+</div>
