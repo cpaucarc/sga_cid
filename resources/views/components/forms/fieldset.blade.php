61b4a05883c787c1cbbb8dfe6c92fb4967ef89ac
Frank Paucar <fcpaucar@gmail.com>
2022-08-03 14:43:27 -0500

    :sparkles: Agrega el componente de fieldset

---
+++ resources/views/components/forms/fieldset.blade.php
@@ -0,0 +1,8 @@
+<fieldset class="bg-slate-50/50 border border-slate-100 px-5 py-4 rounded-md">
+    <legend class="text-slate-700 text-sm font-bold bg-slate-100 px-3 py-1 rounded">
+        {{ $titulo }}
+    </legend>
+
+    {{ $slot }}
+
+</fieldset>
