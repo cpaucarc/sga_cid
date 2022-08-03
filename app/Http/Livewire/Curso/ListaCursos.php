be801fd627d604553ddecf42725d0552a78a3127
Frank Paucar <fcpaucar@gmail.com>
2022-08-02 19:16:51 -0500

    :sparkles: Agrega la lista de cursos

--- app/Http/Livewire/Curso/ListaCursos.php
+++ app/Http/Livewire/Curso/ListaCursos.php
@@ -11,7 +11,8 @@ use Livewire\Component;
 
 class ListaCursos extends Component
 {
-    public $dictables = null, $dictable_id = null; // idioma_dictable_id
+    public $dictable = null, $dictable_id = null, $requisito = null;
+    public $ciclos = null;
     public $niveles = null, $nivel = 0;
     public $idiomas = null, $idioma = 1;
     public $modalidades = null, $modalidad = 0;
@@ -19,11 +20,17 @@ class ListaCursos extends Component
     public function mount($id = null)
     {
         $this->dictable_id = $id;
+        $this->ciclos = Constants::ciclos()->pluck('nombre', 'id')->all();
         $this->niveles = Constants::idioma_niveles()->pluck('nombre', 'id')->all();
         $this->idiomas = Idioma::query()->orderBy('nombre')->get()->pluck('nombre', 'id')->all();
         $this->modalidades = Modalidad::query()->orderBy('nombre')->get()->pluck('nombre', 'id')->all();
 
-        $this->dictables = IdiomaDictable::query()->find($id);
+        $this->dictable = IdiomaDictable::query()->find($id);
+
+        if (!is_null($this->dictable) && !is_null($this->dictable->requisito)) {
+            $this->requisito = IdiomaDictable::query()->where('codigo', $this->dictable->requisito)->first();
+        }
+
     }
 
     public function render()
@@ -31,7 +38,7 @@ class ListaCursos extends Component
         $cursos = Curso::query();
 
         if (!is_null($this->dictable_id)) {
-            $cursos = $cursos->where('idioma_dictable_id', $this->id);
+            $cursos = $cursos->where('idioma_dictable_id', $this->dictable_id);
         }
 
         $cursos = $cursos->get();
--- resources/views/livewire/curso/lista-cursos.blade.php
+++ resources/views/livewire/curso/lista-cursos.blade.php
@@ -1,30 +1,73 @@
-<div class="space-y-4">
-    Be like water.
+<div class="space-y-6">
 
-    {{ $dictables }}
+    <x-titulo
+        titulo="Idioma: {{ $idiomas[$dictable->idioma_id] }} {{ $niveles[$dictable->idioma_nivel_id] }} - {{ $modalidades[$dictable->modalidad_id] }}">
+        @slot('subtitulo')
+            <div class="rounded-md text-sm text-slate-700 flex flex-wrap gap-6 mt-2 relative">
+                @if(!is_null($requisito))
+                    <a href="{{ route('curso.cursos', $requisito->id) }}"
+                       class="text-blue-600 hover:text-blue-700 hover:underline soft-transition">
+                        <b>Pre-requisito:</b> {{ $idiomas[$requisito->idioma_id] . ' ' .  $niveles[$requisito->idioma_nivel_id] . ' - ' . $modalidades[$requisito->modalidad_id] }}
+                    </a>
+                @else
+                    <p><b>Pre-requisito:</b> Ninguno</p>
+                @endif
 
-    <div class="flex space-x-2 items-center justify-end">
-        <x-forms.select wire:model="idioma">
-            <option value="0">Todos los idiomas</option>
-            @foreach($idiomas as $idi => $idm)
-                <option value="{{ $idi }}">{{ $idm }}</option>
-            @endforeach
-        </x-forms.select>
+                <p><b>Duracion:</b> {{ $dictable->duracion_meses }} meses</p>
 
-        <x-forms.select wire:model="nivel">
-            <option value="0">Todos los niveles</option>
-            @foreach($niveles as $idn => $nv)
-                <option value="{{ $idn }}">Nivel {{ $nv }}</option>
-            @endforeach
-        </x-forms.select>
+                <p><b>Precio mensual:</b> S/. {{ $dictable->precio_mensual }}</p>
+            </div>
+        @endslot
+    </x-titulo>
 
-        <x-forms.select wire:model="modalidad">
-            <option value="0">Todas las modalidades</option>
-            @foreach($modalidades as $idm => $mdl)
-                <option value="{{ $idm }}">{{ $mdl }}</option>
-            @endforeach
-        </x-forms.select>
+
+    <div class="space-y-2">
+        <x-alerta>
+            Este idioma esta declarado con una duración de <b>{{ $dictable->duracion_meses }} meses</b>, por lo que
+            deberia de contar con <b>{{ $dictable->duracion_meses }} cursos</b>, sin
+            embargo, actualmente tiene <b>{{ count($cursos) }} cursos registrados.</b>
+        </x-alerta>
+
+        <div class="flex justify-center gap-x-4">
+            <x-jet-secondary-button class="btn-state-danger">
+                Agregar cursos automaticamente
+            </x-jet-secondary-button>
+
+            <x-jet-secondary-button class="btn-state-default">
+                Agregar curso manualmente
+            </x-jet-secondary-button>
+        </div>
+        <br>
     </div>
 
-    {{ $cursos }}
+
+    <x-table.table>
+        @slot('head')
+            <x-table.head>Código</x-table.head>
+            <x-table.head>Pre-requisito</x-table.head>
+            <x-table.head>Idioma</x-table.head>
+            <x-table.head class="text-center">Ciclo</x-table.head>
+            <x-table.head>Max. por grupo</x-table.head>
+            <x-table.head><span class="sr-only">Acciones</span></x-table.head>
+        @endslot
+
+        @foreach($cursos as $curso)
+            <x-table.row>
+                <x-table.column class="uppercase">{{ $curso->codigo }}</x-table.column>
+                <x-table.column class="uppercase">{{ $curso->requisito ?? '---' }}</x-table.column>
+                <x-table.column>
+                    <b>{{ $idiomas[$dictable->idioma_id] }}</b> - {{ $niveles[$dictable->idioma_nivel_id] }}
+                </x-table.column>
+                <x-table.column class="text-center"><b>{{ $ciclos[$curso->ciclo_id] }}</b></x-table.column>
+                <x-table.column>{{ $curso->aforo_maximo }} estudiantes</x-table.column>
+                <x-table.column>
+                    <x-links.secondary href="#" class="btn-state-transparent">
+                        Editar
+                    </x-links.secondary>
+                </x-table.column>
+            </x-table.row>
+        @endforeach
+
+    </x-table.table>
+
 </div>
