a093cc3a1cd97206b6d58149a68422807e1f29d2
Frank Paucar <fcpaucar@gmail.com>
2022-08-03 09:09:14 -0500

    :lipstick: Agrega las clases para cambiar el color del Toast

--- public/js/alertas.js
+++ public/js/alertas.js
@@ -3,8 +3,12 @@ function sweetToast(mensaje, icon = 'success') {
         toast: true,
         position: 'bottom-end',
         showConfirmButton: false,
-        timer: 3000,
+        timer: 3500,
         timerProgressBar: true,
+        iconColor: '#1e293b',
+        customClass: {
+            popup: 'colored-toast'
+        },
         didOpen: (toast) => {
             toast.addEventListener('mouseenter', Swal.stopTimer)
             toast.addEventListener('mouseleave', Swal.resumeTimer)
