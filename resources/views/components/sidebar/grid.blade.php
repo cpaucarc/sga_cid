3dcd53f71e67a235237d60a5c9fd977b23c7a961
Frank Paucar <fcpaucar@gmail.com>
2022-08-03 09:51:50 -0500

    :lipstick: Asigna la clase sticky al navbar

--- resources/views/components/sidebar/grid.blade.php
+++ resources/views/components/sidebar/grid.blade.php
@@ -1,5 +1,5 @@
 <div class="grid grid-cols-10 gap-6 items-start">
-    <div class="col-span-2 sticky top-8">
+    <div class="col-span-2 sticky top-20">
         <div class="flex flex-col items-start gap-y-6 divide-slate-200 divide-y">
             {{ $sidebar }}
         </div>
