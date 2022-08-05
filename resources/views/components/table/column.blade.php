138a7177ca11efa5061ba585dd8a51f733310002
Frank Paucar <fcpaucar@gmail.com>
2022-08-05 10:00:10 -0500

    :sparkles: Agrega la propiedad hide a los componentes head y column

--- resources/views/components/table/column.blade.php
+++ resources/views/components/table/column.blade.php
@@ -1,3 +1,13 @@
-<td {{ $attributes->merge(['class' => 'px-4 py-3']) }}>
+@props(['hide'])
+
+@php
+    $clases = (isset($hide) && $hide) ? [
+        'sm' => ' hidden sm:table-cell ',
+        'md' => ' hidden md:table-cell ',
+        'lg' => ' hidden lg:table-cell '
+    ][$hide == 1 ? 'lg' : $hide] : '';
+@endphp
+
+<td {{ $attributes->merge(['class' => 'px-4 py-3' . $clases]) }}>
     {{ $slot }}
 </td>
--- resources/views/components/table/head.blade.php
+++ resources/views/components/table/head.blade.php
@@ -1,3 +1,13 @@
-<th {{ $attributes->merge(['scope' => 'col', 'class' => 'px-4 py-2 text-xs font-semibold text-slate-600 uppercase tracking-wider']) }}>
+@props(['hide'])
+
+@php
+    $clases = (isset($hide) && $hide) ? [
+        'sm' => ' hidden sm:table-cell ',
+        'md' => ' hidden md:table-cell ',
+        'lg' => ' hidden lg:table-cell '
+    ][$hide == 1 ? 'lg' : $hide] : '';
+@endphp
+
+<th {{ $attributes->merge(['scope' => 'col', 'class' => 'px-4 py-2 text-xs font-semibold text-slate-600 uppercase tracking-wider' . $clases]) }}>
     {{ $slot }}
 </th>
