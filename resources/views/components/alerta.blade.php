3c7b6b57f10952e185853e6d3a0286c776ffa40e
Frank Paucar <fcpaucar@gmail.com>
2022-08-04 19:15:23 -0500

    :lipstick: Agrega la opcion de indicar el tipo de alerta

--- resources/views/components/alerta.blade.php
+++ resources/views/components/alerta.blade.php
@@ -1,5 +1,18 @@
-<div class="bg-amber-300 rounded-md px-4 py-3 flex items-center gap-x-4 text-sm text-slate-900">
-    <x-icons.warning class="icon-6"/>
+@props(['tipo' => 'warning'])
+
+@php
+    $clase = [
+        'warning' => 'bg-amber-300',
+        'danger' => 'bg-rose-200',
+        'success' => 'bg-lime-200',
+        'info' => 'bg-blue-200',
+    ][$tipo ?? 'warning']
+@endphp
+
+<div class="{{ $clase }} rounded-md px-4 py-3 flex items-center gap-x-4 text-sm text-slate-900">
+    @if($tipo == 'warning')
+        <x-icons.warning class="icon-6"/>
+    @endif
     <div {{ $attributes }}>
         {{ $slot }}
     </div>
