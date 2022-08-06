1d9e6ea8435feffc4feaef4ddbe2931351baf4e3
Frank Paucar <fcpaucar@gmail.com>
2022-08-05 21:25:25 -0500

    :lipstick: Cambia el diseño del login

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,9 +1,12 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
+<div class="min-h-screen pt-10 bg-slate-100">
+    <div class="flex flex-col sm:justify-center items-center">
+        <div>
+            {{ $logo }}
+        </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+        <div
+            class="w-10/12 sm:w-8/12 md:w-6/12 lg:w-3/12 border border-slate-300 mt-6 px-6 pt-6 pb-4 bg-white overflow-hidden rounded-md">
+            {{ $slot }}
+        </div>
     </div>
 </div>
