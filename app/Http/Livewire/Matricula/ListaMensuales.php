fdf0f34c2147db1db35523ad73028b9a3cf5eb35
Frank Paucar <fcpaucar@gmail.com>
2022-08-06 16:33:20 -0500

    :sparkles: Permite personalizar el uso de componente lista-mensuales

--- app/Http/Livewire/Matricula/ListaMensuales.php
+++ app/Http/Livewire/Matricula/ListaMensuales.php
@@ -7,25 +7,31 @@ use Livewire\Component;
 
 class ListaMensuales extends Component
 {
-    public $meses = null, $mes = 0, $year;
+    public $meses = null, $mes = 0, $year, $ruta;
 
-    public function mount($mes, $meses, $year)
+    public function mount($meses, $year, $ruta = "#", $mes = null)
     {
         $this->year = $year;
         $this->meses = $meses;
-        $this->mes = $mes;
+        $this->mes = $mes ?? 0;
+        $this->ruta = $ruta;
     }
 
     public function render()
     {
-        $mensuales = DB::table('mensuales')->select('id', 'mes_id')->where('anio', $this->year)->get();
+        $mensuales = DB::table('mensuales')->select('id', 'mes_id')->where('anio', $this->year)->orderBy('mes_id')->get();
         return view('livewire.matricula.lista-mensuales', compact('mensuales'));
+    }
+
+    public function updatedYear($value)
+    {
+        $this->mes = 0;
     }
 
     public function updatedMes($value)
     {
         if ($value > 0) {
-            return redirect()->route('matriculas.prematricula.director', ['year' => $this->year, 'month' => $value]);
+            return redirect()->route($this->ruta, ['year' => $this->year, 'month' => $value]);
         }
     }
 }
--- resources/views/livewire/matricula/lista-mensuales.blade.php
+++ resources/views/livewire/matricula/lista-mensuales.blade.php
@@ -1,6 +1,9 @@
 <div class="flex gap-x-2 items-center">
     <x-jet-input type="number" min="2010" max="{{ now()->year }}" step="1" wire:model="year"/>
     <x-forms.select wire:model="mes">
+        @if(count($mensuales))
+            <option value="0">Seleccione</option>
+        @endif
         @forelse($mensuales as $msl)
             <option value="{{ $msl->mes_id }}">{{ $meses[$msl->mes_id] }}</option>
         @empty
