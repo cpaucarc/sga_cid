7ed423c4b854f0d89badad08d4699ea8ef95ca25
Frank Paucar <fcpaucar@gmail.com>
2022-08-05 21:33:28 -0500

    :sparkles: Muestra los valores de los aforos minimo, recomendado en la lista de cursos

--- app/Http/Livewire/Curso/AgregarCurso.php
+++ app/Http/Livewire/Curso/AgregarCurso.php
@@ -25,10 +25,16 @@ class AgregarCurso extends Component
     public $modalidad = "";
 
     public $codigo = "";
-    public $aforo = 25;
+    public $aforo_minimo = 15, $aforo_recomendado = 20, $aforo_maximo = 25;
     public $nombre_requisito = "Ninguno";
 
     public $listeners = ['abrirModalNuevo', 'abrirModalEditar'];
+
+    protected $rules = [
+      'aforo_minimo' => 'required|lte:aforo_recomendado|gte:0',
+      'aforo_recomendado' => 'required|lte:aforo_maximo|gte:aforo_minimo',
+      'aforo_maximo' => 'required|gte:aforo_recomendado'
+    ];
 
     public function mount($idioma_dictable_id, $idioma, $modalidad, $nivel)
     {
@@ -51,7 +57,10 @@ class AgregarCurso extends Component
         $this->codigo = substr(Str::uuid(), 0, 8);
         $this->curso_ultimo = Curso::query()->where('idioma_dictable_id', $this->idioma_dictable_id)->orderBy('ciclo_id', 'desc')->first();
         $this->ciclo = $this->curso_ultimo ? (intval($this->curso_ultimo->ciclo_id) + 1) : 1;
-        $this->aforo = $this->curso_ultimo ? $this->curso_ultimo->aforo_maximo : 25;
+
+        $this->aforo_minimo = $this->curso_ultimo ? $this->curso_ultimo->aforo_minimo : 15;
+        $this->aforo_recomendado = $this->curso_ultimo ? $this->curso_ultimo->aforo_recomendado : 20;
+        $this->aforo_maximo = $this->curso_ultimo ? $this->curso_ultimo->aforo_maximo : 25;
 
         $this->nombre_requisito = $this->curso_ultimo ? \App\Models\Curso::nombre_completo($this->curso_ultimo->id) : 'Ninguno';
         $this->open = true;
@@ -65,7 +74,10 @@ class AgregarCurso extends Component
         $this->codigo = $this->curso_actual->codigo;
         $this->curso_ultimo = Curso::query()->where('codigo', $this->curso_actual->requisito)->first();
         $this->ciclo = $this->curso_actual->ciclo_id;
-        $this->aforo = $this->curso_actual->aforo_maximo;
+
+        $this->aforo_minimo = $this->curso_ultimo ? $this->curso_ultimo->aforo_minimo : 15;
+        $this->aforo_recomendado = $this->curso_ultimo ? $this->curso_ultimo->aforo_recomendado : 20;
+        $this->aforo_maximo = $this->curso_ultimo ? $this->curso_ultimo->aforo_maximo : 25;
 
         $this->nombre_requisito = $this->curso_ultimo ? \App\Models\Curso::nombre_completo($this->curso_ultimo->id) : 'Ninguno';
         $this->open = true;
@@ -73,9 +85,12 @@ class AgregarCurso extends Component
 
     public function guardarCurso()
     {
+        $this->validate();
 
         if ($this->editar){
-            $this->curso_actual->aforo_maximo = $this->aforo;
+            $this->curso_actual->aforo_minimo = $this->aforo_minimo;
+            $this->curso_actual->aforo_recomendado = $this->aforo_recomendado;
+            $this->curso_actual->aforo_maximo = $this->aforo_maximo;
             $this->curso_actual->save();
             $this->emit('guardado', 'El curso fue modificado correctamente.');
         }else{
@@ -83,7 +98,9 @@ class AgregarCurso extends Component
                 'codigo' => $this->codigo,
                 'requisito' => $this->curso_ultimo?->codigo,
                 'ciclo_id' => $this->ciclo,
-                'aforo_maximo' => $this->aforo,
+                'aforo_minimo' => $this->aforo_minimo,
+                'aforo_recomendado' => $this->aforo_recomendado,
+                'aforo_maximo' => $this->aforo_maximo,
                 'idioma_dictable_id' => $this->idioma_dictable_id,
             ]);
             $this->emit('guardado', 'El curso fue registrado correctamente.');
