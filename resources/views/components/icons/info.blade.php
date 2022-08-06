2893379d97baeef8b662210872d58bdb4db27ada
Frank Paucar <fcpaucar@gmail.com>
2022-08-06 16:25:33 -0500

    :bento: Agrega los iconos de academic, dollar, info y star

---
+++ resources/views/components/icons/info.blade.php
@@ -0,0 +1,6 @@
+@props(['stroke' => 2])
+
+<svg {{ $attributes->merge(['class' => '']) }} fill="none" viewBox="0 0 24 24" stroke="currentColor"
+     stroke-width="{{ $stroke }}">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+</svg>
