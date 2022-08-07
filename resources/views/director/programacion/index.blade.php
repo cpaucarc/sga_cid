b33b9ea732044b9d90c1f0d0e8d1a0470593c09f
Frank Paucar <fcpaucar@gmail.com>
2022-08-07 13:31:50 -0500

    :bug: Oculta el selector de años y meses si no hay ningun registro en mensuales

--- resources/views/director/programacion/index.blade.php
+++ resources/views/director/programacion/index.blade.php
@@ -7,13 +7,15 @@
 
         <x-titulo titulo="{{ 'Meses de ' . $year }}">
             @slot('items')
-                <livewire:matricula.lista-mensuales mes="{{ $mensual->mes_id }}" year="{{ $mensual->anio }}"
-                                                    :meses="$meses" ruta="director.matricula.programacion"/>
+                @if($mensual)
+                    <livewire:matricula.lista-mensuales mes="{{ $mensual->mes_id }}" year="{{ $mensual->anio }}"
+                                                        :meses="$meses" ruta="director.matricula.programacion"/>
+                @endif
                 <x-links.primary href="{{ route('programacion.mensual.crear') }}">Registrar nuevo</x-links.primary>
             @endslot
         </x-titulo>
 
-        <div class="grid grid-cols-9 gap-8">
+        <div class="grid grid-cols-9 gap-4">
             <div class="col-span-6">
                 <livewire:programacion.lista-meses :meses="$meses" :year="$year"
                                                    :clase_modalidades="$clase_modalidades"/>
