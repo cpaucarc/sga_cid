6a61b0655b9f7a15f2fa599638bd243b78b952d8
Frank Paucar <fcpaucar@gmail.com>
2022-08-10 16:49:11 -0500

    :lipstick: Cambia el sistema de diseño a los estilos de Atlassian DS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -10,9 +11,48 @@ module.exports = {
     ],
 
     theme: {
+        fontSize: {
+            'xs': '13px',
+            'sm': '14px',
+            'base': '16px',
+            'lg': '18px',
+            'xl': '20px',
+            '2xl': '24px',
+            '3xl': '30px',
+            '4xl': '36px',
+            '5xl': '48px',
+        },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            blue: {
+                3: '#0065FF',
+                4: '#0747A6',
+            },
+            amber: {
+                3: '#FFAB00',
+                4: '#FF8B00',
+            },
+            rose: {
+                3: '#E11D48',
+                4: '#BE123C',
+            },
+            green: {
+                3: '#00875A',
+                4: '#006644',
+            },
+            gray: {
+                1: '#DCDDE2',
+                2: '#7A869A',
+                3: '#253858',
+                4: '#091E42',
+            },
+        },
         extend: {
             fontFamily: {
-                sans: ['Albert Sans', ...defaultTheme.fontFamily.sans],
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
             },
         },
     },
